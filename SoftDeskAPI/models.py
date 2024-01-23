3f216b4fad26641dcb14bd849d99240b19608b8f
Deborah Druine <deborahdruine@gmail.com>
2024-01-23 14:08:04 +0100

    mise en place des tokens

--- SoftDeskAPI/models.py
+++ SoftDeskAPI/models.py
@@ -13,11 +13,16 @@ class Project (models.Model):
     ]
 
     author_project = models.ForeignKey(User, on_delete=models.CASCADE, related_name='authored_projects')
-    contributors = models.ManyToManyField(User, related_name='contributed_projects')
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
     type = models.CharField(max_length=10, choices=TYPE_CHOICES)
     created_on = models.DateTimeField(auto_now_add=True)
+    contributors = models.ManyToManyField(User, through='Contributor', related_name='projects')
+
+
+class Contributor (models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
 
 
 class Issue (models.Model):
@@ -41,7 +46,6 @@ class Issue (models.Model):
 
     author_issue = models.ForeignKey(User, on_delete=models.CASCADE, related_name='authored_issues')
     project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='project_issues')
-    issue_contrib = models.ManyToManyField(User, related_name='contributed_issues')
     title = models.CharField(max_length=255)
     body = models.TextField(blank=True, null=True)
     priority = models.CharField(max_length=15, choices=PRIORITY_CHOICES)
@@ -53,6 +57,7 @@ class Issue (models.Model):
 class Comment(models.Model):
     uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
     issue = models.ForeignKey(Issue, on_delete=models.CASCADE)
-    author_comment = models.ForeignKey(User, on_delete=models.CASCADE)
+    author_comment = models.ForeignKey(Contributor, on_delete=models.CASCADE)
     description = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
+    # faire le lien
